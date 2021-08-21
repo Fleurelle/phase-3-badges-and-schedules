be6f500337f63f19d55f4133773dd040088841ee
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-08-21 19:34:34 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker (names)
+    return "Hello, my name is #{names}."
+end 
+
+def batch_badge_creator(array)
+    array.map do |names|
+       "Hello, my name is #{names}."
+    end
+end
+
+def assign_rooms(array)
+    # counter = 0
+    # see https://stackoverflow.com/questions/20040757/using-each-with-index-with-map/20041563 for guidance 
+    array.map.with_index(1) do |names, roomNum|
+       "Hello, #{names}! You'll be assigned to room #{roomNum}!"
+        # counter +=1
+    end
+end
+
+def printer(array)
+   batch_badge_creator(array).each do |name|
+    puts name 
+   end
+
+   assign_rooms(array).each do |room|
+    puts room
+   end
+end
